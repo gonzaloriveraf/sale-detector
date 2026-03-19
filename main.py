bd560478112296d49b4b862530cd38314425060f
gonzaloriveraf <36455946+gonzaloriveraf@users.noreply.github.com>
2026-03-19 12:59:15 +0000

    feat: conexion supabase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from scraper import scrape_falabella
+from database import get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -11,6 +14,16 @@ class ScrapeRequest(BaseModel):
 def root():
     return {"status": "ok"}
 
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    result = db.execute("SELECT 1").fetchone()
+    return {"db": "conectada", "result": result[0]}
+
+
 @app.post("/scrape")
 def scrape(request: ScrapeRequest):
     return scrape_falabella(request.url)
+
+
+
+

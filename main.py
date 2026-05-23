61a300d13cba7781de81f9f1c01e44feda7f7bae
gonzaloriveraf <36455946+gonzaloriveraf@users.noreply.github.com>
2026-05-23 00:01:01 +0000

    :

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ def list_productos(db: Session = Depends(get_db)):
     return get_productos(db)
 
 @app.get("/run-cronjob")
-def run_cronjob()
+def run_cronjob():
     result = run_cronjob()
     return rresult
